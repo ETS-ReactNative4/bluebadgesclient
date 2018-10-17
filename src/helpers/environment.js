c88b92a2356bd82670287307df1ca65090f80806
Aurelia Carlin <aricarlin@gmail.com>
2018-10-17 16:04:04 -0400

    take4

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:4000';
         break;
     // this is the deployed react application
-    case 'https://constructorclient.herokuapp.com':
+    case 'constructorclient.herokuapp.com':
         // this is the full url of your deployed API
         APIURL = 'https://mystoryconstructor.herokuapp.com'
 }
