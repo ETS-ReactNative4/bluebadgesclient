668915e3f31aad3abd1f07fbd2d9d4b92bda417c
Aurelia Carlin <aricarlin@gmail.com>
2018-10-17 14:38:36 -0400

    take 2

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,9 +7,9 @@ switch (window.location.hostname) {
         APIURL = 'http://localhost:4000';
         break;
     // this is the deployed react application
-    case 'https://constructorclient.herokuapp.com/':
+    case 'https://constructorclient.herokuapp.com':
         // this is the full url of your deployed API
-        APIURL = 'https://mystoryconstructor.herokuapp.com/'
+        APIURL = 'https://mystoryconstructor.herokuapp.com'
 }
 
 export default APIURL;
